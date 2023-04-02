d1d489a8d7e0e1fe932e4c4efc3b20ff44d06796
Talibat 3ilm <omnia.hefzy@gmail.com>
2023-04-03 00:44:24 +0300

    updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,11 @@ import React from 'react'
 function Card(props) {
 
   return (
-<<<<<<< HEAD
-    <main className='wrapper'>
-        <article className='card'>
-          <h1 className='cardHead'>{props.heading}</h1>
-          <p className='paragraph'>{props.paragraph}</p>
-          <img src={props.image} alt={props.alt} />
-        </article>
-    </main>
-=======
     <article className='card'>
       <h1 className='cardHead'>{props.heading}</h1>
       <p className='paragraph'>{props.paragraph}</p>
       <img src={props.image} alt={props.alt} />
     </article>
->>>>>>> 9757263 (updates)
   )
 }
 
